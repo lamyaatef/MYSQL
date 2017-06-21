9fd5f59de8dcdfe091da81f641a96e8471a12e05
lamya atef <lamya.atef@sandcti.com>
2017-06-21 12:19:25 +0200

    MySQLUtil - worked with HSSF - final

--- src/mysqlexportapp/MySQLExportApp.java
+++ src/mysqlexportapp/MySQLExportApp.java
@@ -86,62 +86,26 @@ public class MySQLExportApp {
             //Workbook workbook = new XSSFWorkbook();
             Workbook workbook = new HSSFWorkbook();
             Sheet worksheet = workbook.createSheet("My Worksheet");
-            Row rowhead=   worksheet.createRow((short)0);
-            //ArrayList<Row> rows = new ArrayList<Row>();
-            //ArrayList<ArrayList<Cell>> cells=new ArrayList<ArrayList<Cell>>();
-            
-            
-            
             FileOutputStream fileOut = new FileOutputStream(fileName);
-            
-            /*for(int i=1; i<=count;i++){
-                
-                ArrayList<Cell> cell = new ArrayList<Cell>();
-                cells.add(cell);
-            }*/
-            for(int header=0;header<count;header++){
-            
-                rowhead.createCell((short) header).setCellValue(rsMetaData.getColumnName(header+1));
-                      
-            }
-            
-            
-            //int i=0;
             int i=0;
             while(res.next())
             {
-             Row row=   worksheet.createRow((short)i);
-             for(int k=0;k<count;k++)
-                row.createCell((short) k).setCellValue(res.getString(k+1));
-             /* rows.add(worksheet.createRow(i));
-              
-              for(int cellno=0;cellno<count;cellno++){
-                    cells.get(cellno).add(rows.get(i).createCell((short) cellno));
-                }
-              
-             
-              
-                            
-             for (int j = 1 ; j <= count; j ++){
-                    
-                cells.get(j-1).get(i).setCellValue(res.getString(j));
-                    
-             }*/
-              
-            
-              
-              i++;
+                Row row = worksheet.createRow((short)i);
+                for(int k=0;k<count;k++)
+                   row.createCell((short) k).setCellValue(res.getString(k+1));
+                i++;
             }
        
     
            
-
+          worksheet.shiftRows(0, worksheet.getPhysicalNumberOfRows(), 1);
+          System.out.println("worksheet.getFirstRowNum() "+worksheet.getFirstRowNum());
+          Row rowhead=   worksheet.createRow((short)worksheet.getFirstRowNum()-1);
+          for(int header=0;header<count;header++){
             
-          /* for(int header=0;header<count;header++){
-                    cells.get(header).get(0).setCellValue(rsMetaData.getColumnName(header+1));
+                rowhead.createCell((short) header).setCellValue(rsMetaData.getColumnName(header+1));
                       
-                }*/
-          
+          }
           
           workbook.write(fileOut);
 
@@ -153,6 +117,89 @@ public class MySQLExportApp {
         }
         
     }
+    
+    /*private static void copyRow(HSSFWorkbook workbook, HSSFSheet worksheet, int sourceRowNum, int destinationRowNum) {
+        // Get the source / new row
+        HSSFRow newRow = worksheet.getRow(destinationRowNum);
+        HSSFRow sourceRow = worksheet.getRow(sourceRowNum);
+
+        // If the row exist in destination, push down all rows by 1 else create a new row
+        if (newRow != null) {
+            worksheet.shiftRows(destinationRowNum, worksheet.getLastRowNum(), 1);
+        } else {
+            newRow = worksheet.createRow(destinationRowNum);
+        }
+
+        // Loop through source columns to add to new row
+        for (int i = 0; i < sourceRow.getLastCellNum(); i++) {
+            // Grab a copy of the old/new cell
+            HSSFCell oldCell = sourceRow.getCell(i);
+            HSSFCell newCell = newRow.createCell(i);
+
+            // If the old cell is null jump to next cell
+            if (oldCell == null) {
+                newCell = null;
+                continue;
+            }
+
+            // Copy style from old cell and apply to new cell
+            HSSFCellStyle newCellStyle = workbook.createCellStyle();
+            newCellStyle.cloneStyleFrom(oldCell.getCellStyle());
+            ;
+            newCell.setCellStyle(newCellStyle);
+
+            // If there is a cell comment, copy
+            if (newCell.getCellComment() != null) {
+                newCell.setCellComment(oldCell.getCellComment());
+            }
+
+            // If there is a cell hyperlink, copy
+            if (oldCell.getHyperlink() != null) {
+                newCell.setHyperlink(oldCell.getHyperlink());
+            }
+
+            // Set the cell data type
+            newCell.setCellType(oldCell.getCellType());
+
+            // Set the cell data value
+            switch (oldCell.getCellType()) {
+                case Cell.CELL_TYPE_BLANK:
+                    newCell.setCellValue(oldCell.getStringCellValue());
+                    break;
+                case Cell.CELL_TYPE_BOOLEAN:
+                    newCell.setCellValue(oldCell.getBooleanCellValue());
+                    break;
+                case Cell.CELL_TYPE_ERROR:
+                    newCell.setCellErrorValue(oldCell.getErrorCellValue());
+                    break;
+                case Cell.CELL_TYPE_FORMULA:
+                    newCell.setCellFormula(oldCell.getCellFormula());
+                    break;
+                case Cell.CELL_TYPE_NUMERIC:
+                    newCell.setCellValue(oldCell.getNumericCellValue());
+                    break;
+                case Cell.CELL_TYPE_STRING:
+                    newCell.setCellValue(oldCell.getRichStringCellValue());
+                    break;
+            }
+        }
+
+        // If there are are any merged regions in the source row, copy to new row
+        for (int i = 0; i < worksheet.getNumMergedRegions(); i++) {
+            CellRangeAddress cellRangeAddress = worksheet.getMergedRegion(i);
+            if (cellRangeAddress.getFirstRow() == sourceRow.getRowNum()) {
+                CellRangeAddress newCellRangeAddress = new CellRangeAddress(newRow.getRowNum(),
+                        (newRow.getRowNum() +
+                                (cellRangeAddress.getLastRow() - cellRangeAddress.getFirstRow()
+                                        )),
+                        cellRangeAddress.getFirstColumn(),
+                        cellRangeAddress.getLastColumn());
+                worksheet.addMergedRegion(newCellRangeAddress);
+            }
+        }
+    }
+}*/
+    
 /*private static String exportExcelSheetForSMSSendingData(Vector<ArrayList<String>> results,int cellCount,ResultSetMetaData resultMeta,String directionFile)
   {
       System.out.println("inside exportExcelSheetForSMSSendingData "+cellCount);
