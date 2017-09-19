6e9df442aa1e6561692539bfcac32f659ca24488
lamya atef <lamya.atef@sandcti.com>
2017-09-19 17:48:08 +0200

    SQLSDS

--- src/mysqlexportapp/SQLExportSDS.java
+++ src/mysqlexportapp/SQLExportSDS.java
@@ -48,6 +48,8 @@ import org.apache.poi.xssf.usermodel.XSSFRichTextString;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.util.*;
+
 
 /**
  *
@@ -72,35 +74,151 @@ public class SQLExportSDS {
 
 
 
-    private void loopOnTables(Connection con, Connection con2){
+    private static void loadSDSTables(Connection con, Connection con2){
         try{
-        //gvms
+            System.out.println("con "+con.isValid(0));
+            System.out.println("con2 "+con2.isValid(0));
+        //database: gvms at 10.0.0.16
         Statement stat = con.createStatement();
-        //gvms2
+        Statement innerStat = con.createStatement();
+        //database: sds2 at 10.0.0.77
         Statement stat2 = con2.createStatement();
         
         String query="select table_name from user_tables";
         ResultSet rs = stat.executeQuery(query);
+        System.out.println("get tables: "+query);
+        //loop on each table retrieved from gvms
+       // int tables =0;
         while(rs.next())
             {
+            //    System.out.println("table "+tables);
                 String tableName = rs.getString("table_name");
-                //String query3="select * from gvms."+tableName;
-                //ResultSet res = stat.executeQuery(query3);
-                ResultSetMetaData rsMetaData = rs.getMetaData();
-                int count = rsMetaData.getColumnCount();
-                //create table for database gvms2
-                for(int k=0;k<count;k++)
-                {
-                    String columnName = rsMetaData.getColumnName(count);
-                    String columnType = rsMetaData.getColumnTypeName(count);
-                    String query1="create table gvms2."+tableName+" ( "+columnName+" "+columnType+" , "+")";
-                    if(k==count-1)
-                        query1="create table gvms2."+tableName+" ( "+columnName+" "+columnType+" )";
-                    stat2.execute(query1);
-                }
-                    String query2="insert into gvms2."+tableName+"select * from gvms."+tableName;
-                    stat2.execute(query2);
+                String columnName = "";
+                String columnType = "";       
                 
+                String columns="select * from "+tableName;
+                System.out.println("get columns: "+columns);
+                Statement columnsStat = con.createStatement();
+                ResultSet rsColumns = columnsStat.executeQuery(columns);
+        
+                
+                ResultSetMetaData rsMetaData = rsColumns.getMetaData();
+                int count = rsMetaData.getColumnCount();
+                System.out.println("count of table columns: "+tableName+" : "+count);
+                
+                String checkTable = "select * from sds."+tableName;
+                boolean exists = con2.createStatement().execute(checkTable);
+                if(exists)
+                {
+                    String dropQuery="drop table sds."+tableName;
+                    System.out.println("query0-drop "+dropQuery);
+                    con2.createStatement().execute(dropQuery);
+                
+                }
+                
+                String query1="create table sds."+tableName+" ( ";
+                //columns of each table
+                for(int k=1;k<=count;k++)
+                {
+                    columnName = rsMetaData.getColumnName(k);
+                    columnType = rsMetaData.getColumnTypeName(k);
+                    System.out.println("table name "+tableName+" column name "+columnName+" column type "+columnType);
+                    if (columnType.contains("VARCHAR"))
+                            columnType += "(250)";
+                    query1+=columnName+" "+columnType;
+                    if(k==count)
+                        query1+=" ) ";
+                    else
+                        query1+=" , ";
+                    
+                    
+                    
+                }
+                System.out.println("query1 "+query1);
+                stat2.execute(query1);
+                
+                String query2 = "select * from gvms."+tableName+" where rownum<=1000";
+                System.out.println("query2 "+query2);
+                ResultSet data = innerStat.executeQuery(query2);
+                ResultSetMetaData metaData = data.getMetaData();
+                String query3 = "";
+                int totalColumns = metaData.getColumnCount();
+                System.out.println("total columns "+totalColumns);
+                int dataCount = 1;
+                int recordNo = 1;
+                String st="";
+                boolean hasData=false;
+                while(data.next())
+                {
+                    System.out.println("record no "+recordNo);
+                    dataCount = 1;
+                    query3 = "insert into sds."+tableName+" values( ";
+                    while(dataCount<=totalColumns)
+                    {
+                            System.out.println("index: "+dataCount);
+                            hasData=true;
+                            String dateType= metaData.getColumnTypeName(dataCount);
+                            System.out.println("date type "+dateType);
+                            if (dateType.compareToIgnoreCase("DATE")==0)
+                            {
+                                if (st==null)
+                                    query3 +=st;
+                                else
+                                {
+                                    String dateSt = data.getString(metaData.getColumnName(dataCount));
+                                    System.out.println("date st "+dateSt);
+                                    if (dateSt.contains(" "))
+                                        dateSt = dateSt.substring(0, dateSt.indexOf(" "));
+                                    System.out.println("date st After "+dateSt);
+                                    st = "to_date('"+dateSt+"','yyyy-mm-dd')";
+                                    query3 +=st;
+                                }
+
+                            }
+                            else
+                            {
+                                st = data.getString(metaData.getColumnName(dataCount));
+                                System.out.println("st "+st);
+                                if (st!=null && st.contains(" "))
+                                    st = st.trim();
+                                query3 +="'"+st+"'";
+                            }
+
+
+                            if(dataCount==count)
+                            {
+                                query3+=" ) ";
+        //                        break;
+                            }
+
+                            else
+                                query3+=" , ";
+                            System.out.println("query3: "+query3);
+                            dataCount++;
+                    }
+                    if(hasData)
+                        stat2.execute(query3);
+                    recordNo++;
+                }
+               /* if(dataCount==0)
+                {
+                    query3 = "insert into sds."+tableName+" values( ";
+                    for(int i=1;i<=count;i++)
+                    {
+                        query3 +="0";
+                        if(i==count)
+                            query3+=" ) ";
+                    
+                        else
+                            query3+=" , ";
+                    }
+                 
+                }*/
+                
+                //System.out.println("query3 "+query3);
+                /*if(hasData)
+                    stat2.execute(query3);*/
+              //  tables++;
             }
             
             
@@ -121,16 +239,24 @@ public class SQLExportSDS {
             int nSshPort = 60022;                                    // remote SSH host port number
             String strRemoteHost = "10.0.0.16/gvms";  // hostname or ip of your database server
             int nLocalPort = 3306;                                // local port number use to bind SSH tunnel
-            int nRemotePort = 3306;                               // remote port number of your database
+            int nRemotePort = 3306; 
+            // remote port number of your database
             String strDbUser = "gvms";                    // database loging username
             String strDbPassword = "gvms";                    // database login password
 
+            String strDbUser2 = "sds";                    // database loging username
+            String strDbPassword2 = "sds";                    // database login password
+
+            
             Class.forName("oracle.jdbc.driver.OracleDriver");
+            //SID : GVMS
             Connection con = DriverManager.getConnection("jdbc:oracle:thin:@10.0.0.16:1521:gvms",strDbUser,strDbPassword);
-            Connection con2 = DriverManager.getConnection("jdbc:oracle:thin:@10.0.0.16:1521:gvms2",strDbUser,strDbPassword);
+            System.out.println("get con "+con);
+            //SID : SDS
+            Connection con2 = DriverManager.getConnection("jdbc:oracle:thin:@10.0.0.77:1521:sds",strDbUser2,strDbPassword2);
+            System.out.println("get con2 "+con2);
             
-            
-            
+            loadSDSTables(con,con2);
     
 
 
